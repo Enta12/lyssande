f0ff7413c3f9c5c9c789c129f70abf1052f13b47
CONTINI Baptiste <36010397+Enta12@users.noreply.github.com>
2022-05-19 13:30:38 +0200

    fix(lauch.sh): add web800_

--- launch.sh
+++ launch.sh
@@ -5,6 +5,6 @@ docker stop lysande_mysql_db_1
 docker stop lysande_api_1
 docker stop web800_front_1
 docker stop web800_adminer_1
-docker stop mysql_db_1
-docker stop api_1
+docker stop web800_mysql_db_1
+docker stop web800_api_1
 docker-compose up --build -d
