cfa63499e0d50cc8fbee1a5a64c895a30bbd986e
CONTINI Baptiste <36010397+Enta12@users.noreply.github.com>
2022-05-20 16:01:26 +0200

    feat(map): add page

---
+++ front/src/pages/MapPage.tsx
@@ -0,0 +1,15 @@
+import Map from "../components/map/map"
+import fanghMap from '../assets/fangh.jpg';
+import { pjsMocked } from "../moockedData";
+
+
+
+const MapPage = () => {
+
+    return (
+    <Map img={fanghMap} pjs={pjsMocked} mapName={"carte terre de Fangh"}/>
+    )
+   
+}
+
+export default MapPage
