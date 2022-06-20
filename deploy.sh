c0600192e5abeac15be4d49d3b63bb3fbb46a780
CONTINI Baptiste <36010397+Enta12@users.noreply.github.com>
2022-06-20 23:10:18 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,5 @@
 git pull --rebase
-docker stop lysande_front_1
-docker stop lysande_adminer_1
-docker stop lysande_mysql_db_1
-docker stop lysande_api_1
-docker stop web800_front_1
-docker stop web800_adminer_1
-docker stop web800_mysql_db_1
-docker stop web800_api_1
-docker stop terraria
-docker-compose up --build -d
+docker stop lysande
+docker rm lysande
+docker build -t lysande .
+docker run -dit --name lysande --ip 172.18.0.81 --network petit lysande
