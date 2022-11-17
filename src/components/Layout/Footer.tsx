4eb670956ba795040770c814e78c9a7a94c0ef43
CONTINI Baptiste <36010397+Enta12@users.noreply.github.com>
2022-11-17 13:04:29 +0100

    feat(footer): responsive

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
 
 const Footer = () => {
-  return (
-    <footer
-      className="
-        h-12
+	return (
+		<footer
+			className="
+        lg:h-12
+        sm:h-20
+        h-28
         px-16
         bg-swamp
         flex
+        lg:flex-row
+        flex-col
         items-center
-        justify-center
+        justify-around
+        lg:justify-between
         text-white
         font-bubblegum
+        text-center
       "
-    >
-      Fait par Karen, Valentin, Baptiste, Yasmine.
-      Pour toutes questions, veuillez contacter
-      <span className='text-sky-500 underline ml-1'>
-        lyssandecontact@gmail.com
-      </span>
-    </footer>
-  );
+		>
+			Fait par Karen, Valentin, Baptiste, Yasmine.
+			<span className="text-center">
+				<span className="lg:inline md:hidden inline">
+					Pour toutes questions, veuillez contacter
+				</span>
+				<span className="hidden md:inline lg:hidden">Contact : </span>
+				<span className="text-sky-500 underline ml-1">lyssandecontact@gmail.com</span>
+			</span>
+		</footer>
+	);
 };
 
 export default Footer;
